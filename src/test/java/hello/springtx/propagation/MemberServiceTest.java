fd215526384c862d188686605cd41849a3101ade
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-10 14:09:48 +0900

    test(MemberServiceTest): 간단한 MemberService에 대한 테스트 추가

---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+
+    @Autowired MemberRepository memberRepository;
+
+    @Autowired LogRepository logRepository;
+
+    @Test
+    void outerTxOff_success() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+}
