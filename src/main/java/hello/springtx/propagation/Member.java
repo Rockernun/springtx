897fcb29e604ac7f2f2b17f8e0d1d44e63fcd8fc
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-10 14:09:05 +0900

    feat(Member): Member 엔티티 및 리포지토리, 서비스 클래스 추가

---
+++ src/main/java/hello/springtx/propagation/Member.java
@@ -0,0 +1,23 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Member() {}
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/hello/springtx/propagation/MemberRepository.java
@@ -0,0 +1,28 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.EntityManager;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Member member) {
+        log.info("Member 저장");
+        em.persist(member);
+    }
+
+    public Optional<Member> find(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList().stream().findAny();
+    }
+}
